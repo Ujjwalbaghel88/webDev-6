9fbdb27c4ba7d83cc4e22c2991477d67f2013579
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-04-28 18:56:58 +0530

    PracticeForbasicJS

--- JSbasic/script.js
+++ JSbasic/script.js
@@ -2,11 +2,16 @@ console.log("Hello word")
 console.log(2)
 console.log('hhe');
 
-let a=10;
-b=4;
-a=90;
-console.log(a+b);
+// let a=10;
+// b=4;
+// a=90;
+// console.log(a+b);
+// console.log(a);
+
+var a=5;
+a=8;
 console.log(a);
+
 
 
 
