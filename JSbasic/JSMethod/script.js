dd017e4aaf2149f34d90fd80236e5f8ff7dbb1cf
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-05-01 19:48:18 +0530

    FunctionsInJS

---
+++ JSbasic/JSMethod/script.js
@@ -0,0 +1,27 @@
+let x = 20,
+  y = 30;
+
+function sum(x, y) {
+  let t = x + y;
+  console.log(t);
+}
+sum(x, y);
+
+// Function Expression
+const abc = function () {
+  console.log("Hello world");
+};
+
+console.log(abc); //abc that a callBackFunction
+
+abc();
+
+    // const addarrow = (a, b) => {
+    //   return a + b;
+    // };
+    // console.log(addarrow(5, 6));
+
+    // ArrowFunction this here 
+const addarrow = (a, b) => a + b;
+
+console.log(addarrow(5, 6));
