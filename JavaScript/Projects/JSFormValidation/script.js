83381443b32d2ef10eeb063e8a7bca10dc4b46a6
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-05-13 03:50:52 +0530

    Refactor form error handling: update error message elements to use <span> tags and fix phone error reference

--- JavaScript/Projects/JSFormValidation/script.js
+++ JavaScript/Projects/JSFormValidation/script.js
@@ -1,25 +1,5 @@
 function submit() {
-  //  console.log("Submit Button Clicked");
 
-//   const fn = document.getElementById("fullName").value="";
-//   console.log(fn);
-
-//   document.getElementById("fullName").value = "";
-
-//   const pn = document.getElementById("email").value;
-//   console.log(pn);
-
-//   document.getElementById("email").value = "";
-
-//   const gn = document.getElementById("password").value;
-//   console.log(gn);
-
-//   document.getElementById("password").value = "";
-
-//   const hn = document.getElementById("number").value;
-//   console.log(hn);
-
-//   document.getElementById("number").value = "";
   const fn = document.getElementById("fullName").value;
   const em = document.getElementById("email").value;
   const ph = document.getElementById("number").value;
@@ -39,18 +19,7 @@ function submit() {
   }
 
   if (!/^[6-9]\d{9}$/.test(ph)) {
-    document.getElementById("phoneError").innerText = "Invalid Phone";
+    document.getElementById("numberError").innerText = "Invalid Phone";
   }
-
-//   if(fn != "type")
-//   {
-//     console.log("Alert");
-//     // alert("Invalid full name")
-    
-//   }
-//   else{
-//     console.log(fn);
-    
-//   }
 
 }
