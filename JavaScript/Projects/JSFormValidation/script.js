02c66702d9851e5ca27daac8344ee61e31ccaa33
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-05-14 19:54:37 +0530

    RemoveBlankSpaces

--- JavaScript/Projects/JSFormValidation/script.js
+++ JavaScript/Projects/JSFormValidation/script.js
@@ -1,25 +1,21 @@
 function submit() {
-
   const fn = document.getElementById("fullName").value;
   const em = document.getElementById("email").value;
   const ph = document.getElementById("number").value;
- const gn = document.getElementById("password").value;
+  const gn = document.getElementById("password").value;
 
   document.querySelectorAll(".error").forEach((element) => {
     element.innerText = "";
   });
 
-
-  if(!/^[A-Za-z\s]+$/.test(fn))
-  {
-    document.getElementById("nameError").innerText="Invalid name";
+  if (!/^[A-Za-z\s]+$/.test(fn)) {
+    document.getElementById("nameError").innerText = "Invalid name";
   }
-   if (!/^[A-Za-z\.\_\d]+@gmail.com$/.test(em)) {
+  if (!/^[A-Za-z\.\_\d]+@gmail.com$/.test(em)) {
     document.getElementById("emailError").innerText = "Invalid Email";
   }
 
   if (!/^[6-9]\d{9}$/.test(ph)) {
     document.getElementById("numberError").innerText = "Invalid Phone";
   }
-
 }
