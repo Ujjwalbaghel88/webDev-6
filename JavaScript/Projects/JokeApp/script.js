6ffad4878e2ca55e6562b12a447c4b07229ea634
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-05-23 20:41:25 +0530

    Implement joke retrieval functionality with API integration

---
+++ JavaScript/Projects/JokeApp/script.js
@@ -0,0 +1,13 @@
+async function GetNewjoke() {
+  const API_URL =
+    " https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political";
+
+  const response = await fetch(API_URL);
+
+  const data = await response.json();
+  console.log(data);
+
+  document.getElementById("setup").innerText = data.joke || data.setup;
+
+  document.getElementById("punchline").innerText = data.punchline || "";
+}
