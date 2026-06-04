ad5b4cd2eed092ee78716729d316c4a0e58a3515
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-04 23:31:32 +0530

    Add react-icons dependency and update About page to include an icon

--- Frontend/myFirstReactApp/src/pages/About.jsx
+++ Frontend/myFirstReactApp/src/pages/About.jsx
@@ -1,8 +1,11 @@
+import { FaWifi } from "react-icons/fa6";
 function About() {
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">About</span>
+        <span className="text-primary fs-2">
+          <FaWifi /> About
+        </span>
       </div>
     </>
   );
