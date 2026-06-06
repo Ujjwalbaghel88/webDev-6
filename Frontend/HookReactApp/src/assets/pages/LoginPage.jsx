548ebfbfd000f17530f47ccd60febab98d919f5b
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-06 13:14:15 +0530

    Learn A Hook In React

--- Frontend/HookReactApp/src/assets/pages/LoginPage.jsx
+++ Frontend/HookReactApp/src/assets/pages/LoginPage.jsx
@@ -1,7 +1,29 @@
-function LoginPage(){
-    return(
-        <>
-        
-        </>
-    )
+import { useState } from "react";
+
+function LoginPage() {
+  const [userName, setUserName] = useState("");
+  const [city, setCity] = useState("");
+  return (
+    <>
+      <input
+        type="text"
+        name="userName"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <hr /> <br /> <br /> <br />
+      <div>my user Name us {userName}</div>
+      <div>My city Name is {city}</div>
+      <hr />
+      <br />
+      <br />
+      <br />
+      <input
+        type="text"
+        name="city"
+        onChange={(e) => setCity(e.target.value)}
+      />
+    </>
+  );
 }
+
+export default LoginPage;
