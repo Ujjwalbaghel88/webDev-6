1bbc1b27f719b985938aeaef3b7d25e4eaaed589
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-12 17:00:42 +0530

    Styling For Add a Card

--- JavaScript/Projects/FakeStore/src/App.jsx
+++ JavaScript/Projects/FakeStore/src/App.jsx
@@ -1,25 +1,27 @@
-import React from "react"; // its a default import isse kuch nahi hota
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
 import About from "./pages/About";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-function App() {
+const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
-
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
-        <Footer />
+        <Footer/>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- JavaScript/Projects/FakeStore/src/components/Header.jsx
+++ JavaScript/Projects/FakeStore/src/components/Header.jsx
@@ -1,59 +1,43 @@
+import React from "react";
+import { FaStore } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import image1 from "../assets/hero.png"
-
-function Header() {
+const Header = () => {
   return (
     <>
-    <div className="flex bg-green-200 justify-between items-center">
-
-      <Link to={"/"}>Home</Link>
-     </div>
-
-
-      <div
-        id="header"
-        className="flex justify-between items-center shadow-md  bg-[#C2410C]  w-full"
-      >
-        <Link to={"/home"}>
-          <img src={image1} className=" w-15 shrink-0" alt="" />
-
-        </Link>
-      
-        {/* <div className="d-flex gap-4  fs-5 fw-bold">
-          <Link to={"/home"} className="text-white text-decoration-none">
+      <div className="bg-blue-500 flex justify-between  p-3">
+        <div className=" flex">
+          <FaStore className="ms-5 size-7 text-yellow-500" />
+          <span className="text-red-600 ">Fake Store App</span>
+        </div>
+        <div className="flex gap-3">
+          <Link
+            to={"/"}
+            className=" text-pink-500 hover:underline focus:text-yellow-500"
+          >
             Home
           </Link>
-          <Link to={"/about"} className="text-white text-decoration-none">
+          <Link
+            to={"/about"}
+            className=" text-pink-500 hover:underline focus:text-yellow-500"
+          >
             About
           </Link>
-          <Link to={"/contact-us"} className="text-white text-decoration-none">
-            Contact Us
-          </Link>
-          <Link to={"/order"} className="text-white text-decoration-none">
-            Order
-          </Link>
-        </div> */}
-
-        <div className="d-flex align-items-center gap-2 mt-2 mt-sm-0">
           <Link
-            to={"/login"}
-            id="login"
-            className=" px-3 w-100 text-light w-sm-auto"
+            to={"/contactUs"}
+            className=" text-pink-500 hover:underline focus:text-yellow-500"
           >
-            Login
+            ContactUs
           </Link>
           <Link
-            to={"/register"}
-            id="register"
-            className="btn btn-light px-3 w-100 w-sm-auto"
+            to={"/products"}
+            className="  text-pink-500 hover:underline focus:text-yellow-500"
           >
-            Register
+            Products
           </Link>
         </div>
       </div>
-   
-    
     </>
-  )
-}
+  );
+};
+
 export default Header;
