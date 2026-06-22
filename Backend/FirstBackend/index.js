cebb7059b444ac47d79c415fa860e468e0af9075
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-22 20:25:35 +0530

    feat: implement authentication and public routes with controllers

--- Backend/FirstBackend/index.js
+++ Backend/FirstBackend/index.js
@@ -2,15 +2,41 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
 
 const app = express();
+
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+
+//Default APi
+
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
   res.json({ message: "Welcome to my first Backend forever Project" });
 });
-app.post("/login", (req, res) => {
-  res.json({ message: "login Sucessful" });
-});
+
+// app.post("/login", (req, res) => {
+//   res.json({ message: "login Sucessful" });
+// });
+
+// app.get("/logout", (req, res) => {
+//   res.json({ message: "logout Sucessful" });
+// });
+
+// app.post("/register", (req, res) => {
+//   res.json({ message: "register Sucessful" });
+// });
+
+// app.put("/update", (req, res) => {
+//   res.json({ message: "update Sucessful" });
+// });
+
+// app.delete("/delete", (req, res) => {
+//   res.json({ message: "delete Sucessful" });
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+  export const LoginUser = (req, res) => {
+  res.json({ message: "Login successful from controller" });
+};
+
+ export const LogoutUser = (req, res) => {
+  res.json({ message: "Logout successful from controller" });
+};
+
+
+ export const RegisterUser = (req, res) => {
+  res.json({ message: "Register successful from controller" });
+};
