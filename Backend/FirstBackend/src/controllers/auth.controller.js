e6d89f1085dfd0ce7ee14e96d13dd1c32ce0dcfa
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-23 21:04:26 +0530

    feat: implement user registration and add user model

--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -1,12 +1,27 @@
-  export const LoginUser = (req, res) => {
+import User from "../models/user.model";
+export const RegisterUser = async (req, res) => {
+  try {
+    const { fullName, email, password, phone, gender, dob } = req.body;
+
+    if (!fullName || !email || !password || !phone || !gender || !dob) {
+      res.status(400).json({ message: "All field required" });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(409).json({ message: "Email ALready Registered" });
+      return;
+    }
+
+    //create New User And Complere registration will do this tomorrow
+  } catch (error) {}
+};
+
+export const LoginUser = (req, res) => {
   res.json({ message: "Login successful from controller" });
 };
 
- export const LogoutUser = (req, res) => {
+export const LogoutUser = (req, res) => {
   res.json({ message: "Logout successful from controller" });
-};
-
-
- export const RegisterUser = (req, res) => {
-  res.json({ message: "Register successful from controller" });
 };
