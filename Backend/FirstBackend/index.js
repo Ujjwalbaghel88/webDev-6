78900aefe47a9dbf4fef7d12ac36ddaba8f06788
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-06-23 19:58:03 +0530

    db connection

--- Backend/FirstBackend/index.js
+++ Backend/FirstBackend/index.js
@@ -5,12 +5,12 @@ import express from "express";
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
 
+import connectDB from "./src/config/dbConnection.config.js";
 const app = express();
 app.use(express.json());
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
-
 
 //Default APi
 
@@ -42,4 +42,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server Started on port:", port);
+  connectDB();
 });
--- Backend/FirstBackend/src/config/dbConnection.config.js
+++ Backend/FirstBackend/src/config/dbConnection.config.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("MongoDB Connected Succesfully ");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+export default connectDB;
